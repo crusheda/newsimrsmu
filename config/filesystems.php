b9962b0a7cf89fde21feec7620189303becc1d05
Yussuf Faisal <thecrusheda@gmail.com>
2026-06-03 14:55:11 +0700

    change filesystem config

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,8 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/private'),
+            // 'root' => storage_path('app/private'), --- IGNORE ---
+            'root' => storage_path('app/public'),
             'serve' => true,
             'throw' => false,
             'report' => false,
