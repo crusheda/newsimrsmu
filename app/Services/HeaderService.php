aa5e871177a77819270b4b7bdeb90e68c844d3d4
Yussuf Faisal <thecrusheda@gmail.com>
2026-06-11 11:41:26 +0700

    update headerservice

--- app/Services/HeaderService.php
+++ app/Services/HeaderService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class HeaderService
 {
@@ -31,14 +32,76 @@ class HeaderService
             $nama = $user->name;
         }
 
+        $panggilan = $this->getPanggilan(
+            $user->tgl_lahir,
+            $user->jns_kelamin,
+            $user->status_kawin
+        );
+
+        $time = Carbon::now()->isoFormat('H');
+        // PENGHITUNGAN WAKTU PAGI / SIANG / SORE / MALAM
+        if ($time < "10") {
+            $waktu = "Pagi";
+        } else {
+            if ($time >= "10" && $time < "15") {
+                $waktu = "Siang";
+            } else {
+                if ($time >= "15" && $time < "19") {
+                    $waktu = "Sore";
+                } else {
+                    if ($time >= "19") {
+                        $waktu = "Malam";
+                    }
+                }
+            }
+        }
+
         return [
-            'id'    => $user->id,
-            'nama'  => $nama,
-            'email' => $user->email,
-            'role'  => optional($user->roles->first())->name,
-            'foto' => optional($user->foto)->filename
-                    ? asset('storage/' . str_replace('public/', '', $user->foto->filename))
-                    : asset('images/users/user-dummy-img.jpg'),
+            'id'        => $user->id,
+            'waktu'     => $waktu,
+            'nama'      => $nama,
+            'nama_full' => $panggilan . ' ' . $nama,
+            'email'     => $user->email,
+            'role'      => optional($user->roles->first())->name,
+            'foto'      => optional($user->foto)->filename
+                            ? asset('storage/' . str_replace('public/', '', $user->foto->filename))
+                            : asset('images/users/user-dummy-img.jpg'),
         ];
+    }
+
+    private function getPanggilan($tglLahir, $jenisKelamin, $statusKawin): string
+    {
+        if (!$tglLahir) {
+            return '';
+        }
+
+        $umur = Carbon::parse($tglLahir)->age;
+
+        // Usia 50 tahun ke atas
+        if ($umur >= 50) {
+            return $jenisKelamin === 'PEREMPUAN'
+                ? 'Ibu'
+                : 'Bp.';
+        }
+
+        // Usia 35 - 49 tahun
+        if ($umur >= 35) {
+            return $jenisKelamin === 'PEREMPUAN'
+                ? ($statusKawin === 'BELUM' ? 'Nn.' : 'Ny.')
+                : 'Tn.';
+        }
+
+        // Usia di bawah 35 tahun
+        if ($jenisKelamin === 'PEREMPUAN') {
+            return $statusKawin === 'BELUM'
+                ? 'Nn.'
+                : 'Ny.';
+        } else {
+            return $statusKawin === 'BELUM'
+                ? 'Sdr.'
+                : 'Tn.';
+        }
+
+        return 'Sdr.';
     }
 }
--- resources/views/pages/v4/dashboard/index.blade.php
+++ resources/views/pages/v4/dashboard/index.blade.php
@@ -1,5 +1,48 @@
 @extends('layouts.v4')
 
 @section('content')
-{{-- {{ Auth::user() }} --}}
+    <div class="row pt-1">
+        <div class="col-xl-12">
+            <div class="card custom-card podcast-banner-card border-0 shadow-none">
+                <div class="card-body">
+                    <div class="podcast-banner-card-background"> <img src="{{ asset('/images/media/backgrounds/6.png') }}" alt=""> </div>
+                    <div class="row">
+                        <div class="col-xl-8"> <span class="badge bg-primary rounded-pill">Sumber Daya Insani</span>
+                            <h6 class="fw-semibold mt-3">Selamat {{ $headerUser['waktu'] }},</h6>
+                            <h2 class="fw-semibold">{{ $headerUser['nama_full'] }}</h2>
+                            <p class="fs-16 mb-3 text-dark"> Sudahkan Anda Membaca Peraturan Kepegawaian ? </p>
+                            {{-- <div class="fw-medium text-primary mb-4">Telah dibaca sebanyak <i class="ti ti-music mx-1"></i>
+                                <span class="ms-2 text-default fs-12 op-8"><i class="ri-play-circle-line fs-13 mx-1"></i>260.517/ Monthly Listenders</span>
+                            </div> --}}
+                            <div class="btn-list">
+                                <button class="btn btn-lg btn-primary btn-w-lg" data-bs-toggle="modal" data-bs-target="#peraturan-kepegawaian"> Baca Selengkapnya </button>
+                                {{-- <button class="btn btn-lg btn-primary-ghost btn-w-lg">  </button> --}}
+                            </div>
+                            <p class="card-text mt-3"><small class="text-muted">Ditetapkan mulai <cite title="Source Title"><strong><u>1 Juli 2023</u></strong></cite></small></p>
+                        </div>
+                        <div class="col-xl-4">
+                            <img src="{{ asset('/images/pku/all-group-users.png') }}" alt="" class="podcast-banner-img img-fluid d-md-block d-none h-100">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="peraturan-kepegawaian" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h6 class="modal-title" id="exampleModalCenterTitle">Peraturan Kepegawaian <span class="badge text-bg-danger p-1">1 Juli 2023</span></h6>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="_df_book" source="{{ asset('/doc/073_PR_PERATURAN_PERUSAHAAN_2024.pdf') }}"></div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary-transparent" data-bs-dismiss="modal">Tutup</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
